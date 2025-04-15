324228fa44ad47ea9e2d4ace71015ee08439efff
marcuskastevens <marcuskastevens@gmail.com>
2025-04-15 16:28:33 -0400

    Added heads-up poker Kelly optimal bet sizing utility function

--- bet_sizing.py
+++ bet_sizing.py
@@ -17,3 +17,33 @@ def calculate_third_order_kelly_leverage(mu: float, sigma: float, skew: float) -
     See Chapter 9 of "Positional Options Trading" by Euan Sinclar for a derivation.
     """
     return calculate_second_order_kelly_leverage(mu=mu, sigma=sigma) + skew * sigma**3 * mu**2 / (mu**2 + sigma**2) ** 3
+
+
+def calculate_poker_kelly_bet_size(equity: float, pot_size: float, bankroll: float) -> float:
+    """
+    Calculates the heads-up (i.e., 1v1) Kelly-optimal bet size assuming the opponent calls after a bet.
+
+    This is a simplified formula that omits the opponent's fold probability, dynamic post-call equity,
+    the opponent's response curves, and various discretionary elements that impact bet sizing.
+
+    This formula is derived by modeling log wealth as a function of equity, bet size, pot size, and bankroll, and optimizing it with respect to bet size.
+
+    Define log growth function:
+        g(bankroll) = equity * ln(bakroll + pot_size + bet_size) + (1 - equity) * ln(bankroll - bet_size) - ln(bankroll)
+
+    Differentiate with respect to bet size:
+        d_g/d_bet_size = equity / (bakroll + pot_size + bet_size) - (1 - equity) / (bankroll - bet_size)
+
+    Set d_g/d_bet_size = 0 and solve for the optimal bet size:
+        equity / (bakroll + pot_size + bet_size) =  (1 - equity) / (bankroll - bet_size)
+        equity * (bankroll - bet_size) =  (1 - equity) * (bakroll + pot_size + bet_size)
+        equity * bankroll - equity * bet_size =  (1 - equity) * bakroll + (1 - equity) * pot_size + (1 - equity) * bet_size
+        equity * bankroll - (1 - equity) * bakroll - (1 - equity) * pot_size = equity * bet_size + (1 - equity) * bet_size
+        equity * bankroll - (1 - equity) * bakroll - (1 - equity) * pot_size = bet_size * (equity + (1 - equity))
+        --> bet_size = bankroll * (2 * equity - 1) - (1 - equity) * pot_size
+        --> % bet_size = (2 * equity - 1) - (1 - equity) * pot_size / bankroll
+
+    Notice, when the pot size is very large, our optimal bet size shrinks. This is due to the diminishing marginal utility of betting.
+    Strong equity in large pot size situations implies already good pot odds, thus, the marginal benefit we get from risking more is diminished.
+    """
+    return (2 * equity - 1) - (1 - equity) * pot_size / bankroll
